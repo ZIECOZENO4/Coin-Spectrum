8f951f381a66c79e648791ceb6fe064e0325dbd1
ZIECOZENO4 <ziecozeno4@gmail.com>
2025-01-14 16:59:29 +0100

    uhuh

--- app/invest/page.tsx
+++ app/invest/page.tsx
@@ -18,10 +18,7 @@ const InvestPage = () => {
         <div className="mt-10">
                 <Section />
               </div>
-        <div className="px-4 md:px-8">
-            <h1 className="text-3xl text-left font-serif font-bold mb-2 md:mb-4">Trending Videos</h1>
-            <Videos />
-        </div>
+      
         <div className="div">
     <Footer />
   </div>
