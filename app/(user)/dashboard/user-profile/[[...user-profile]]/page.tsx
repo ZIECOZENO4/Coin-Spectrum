ac7b049fb8653fe6b68706ce8410014790cd980f
ZIECOZENO4 <ziecozeno4@gmail.com>
2025-01-18 10:01:00 +0100

    lkjvh

--- app/(user)/dashboard/user-profile/[[...user-profile]]/page.tsx
+++ app/(user)/dashboard/user-profile/[[...user-profile]]/page.tsx
@@ -1,8 +1,8 @@
 import { UserProfile } from "@clerk/nextjs";
  
 const UserProfilePage = () => (
-    <div className=" bg-black w-[100vw] overflow-x-hidden">
-  <UserProfile path="/user-profile" routing="path" />
+    <div className=" bg-black overflow-x-hidden">
+  <UserProfile path="/dashboard/user-profile" routing="path" />
     </div>
 
 );
