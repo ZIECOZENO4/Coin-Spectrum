26b419a43d0ecddbd8578addf44877c09413bb36
ZIECOZENO4 <ziecozeno4@gmail.com>
2025-01-19 18:23:22 +0100

    klm

--- app/(user)/dashboard/makedeposit/DepositCard.tsx
+++ app/(user)/dashboard/makedeposit/DepositCard.tsx
@@ -213,7 +213,7 @@ const DepositCard = () => {
             disabled={isButtonDisabled}
           >
             {isUploading ? (
-              "Uploa"
+              "Uploading...."
             ) : (
               <>
                 <Plus className="mr-2 h-4 w-4" /> Upload Image
