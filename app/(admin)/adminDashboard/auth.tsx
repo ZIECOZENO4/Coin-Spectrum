d732be8406087f9e6629b789bf342d139e417df1
ZIECOZENO4 <ziecozeno4@gmail.com>
2025-01-21 00:15:48 +0100

    p[ojoj

--- app/(admin)/adminDashboard/auth.tsx
+++ app/(admin)/adminDashboard/auth.tsx
@@ -19,15 +19,15 @@ const Auth = () => {
   const adminEmail = process.env.NEXT_PUBLIC_ADMIN_CONTACT_EMAIL;
 
   return (
-    <div id="login" className="w-64 h-80 bg-indigo-50 rounded shadow flex flex-col justify-between p-3">       
-      <form className="text-indigo-500" onSubmit={handleSubmit}>
-        <fieldset className="border-4 border-dotted border-indigo-500 p-5">
+    <div id="login" className="w-64 align-middle justify-center p-4 h-80 bg-black rounded shadow-2xl flex flex-col">       
+      <form className="text-yellow-500" onSubmit={handleSubmit}>
+        <fieldset className="border-4 border-dotted border-yellow-500 p-5">
           <legend className="px-2 italic -mx-2">Welcome admin!</legend>
           <label className="text-xs font-bold after:content-['*'] after:text-red-400" htmlFor="adminId">
             Admin ID 
           </label>     
           <input 
-            className="w-full p-2 mb-2 mt-1 outline-none ring-none focus:ring-2 focus:ring-indigo-500" 
+            className="w-full p-2 mb-2 mt-1 outline-none ring-none focus:ring-2 focus:ring-yellow-500" 
             type="text"
             id="adminId"
             value={adminId}
@@ -38,7 +38,7 @@ const Auth = () => {
             Password  
           </label>
           <input 
-            className="w-full p-2 mb-2 mt-1 outline-none ring-none focus:ring-2 focus:ring-indigo-500" 
+            className="w-full p-2 mb-2 mt-1 outline-none ring-none focus:ring-2 focus:ring-yellow-500" 
             type="password"
             id="password"
             value={password}
@@ -53,7 +53,7 @@ const Auth = () => {
           </a>
           <button 
             type="submit"
-            className="w-full rounded bg-indigo-500 text-indigo-50 p-2 text-center font-bold hover:bg-indigo-400"
+            className="w-full rounded bg-yellow-500 text-yellow-50 p-2 text-center font-bold hover:bg-yellow-400"
           >
             Log In
           </button>
