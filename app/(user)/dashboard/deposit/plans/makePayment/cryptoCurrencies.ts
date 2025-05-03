b6c78c60c3f70433e9a6f6efd7c6e71f333ab301
ZIECOZENO4 <ziecozeno4@gmail.com>
2025-05-03 19:39:54 +0100

    nwibe

--- app/(user)/dashboard/deposit/plans/makePayment/cryptoCurrencies.ts
+++ app/(user)/dashboard/deposit/plans/makePayment/cryptoCurrencies.ts
@@ -1,7 +1,9 @@
 export const cryptocurrencies = [
   { name: "BTC", fullname: "Bitcoin", address: "1LE3uTbMFBhggNbtxhvBxB6aCKpJTHT4cg" },
-  { name: "USDT", fullname: "Tether", address: "0x481A2AD25c61765e26B0A87C69E765261B5BaEF3" },
-  { name: "ETH", fullname: "Ethereum", address: "0x481A2AD25c61765e26B0A87C69E765261B5BaEF3" },
+  // { name: "USDT", fullname: "Tether", address: "0x481A2AD25c61765e26B0A87C69E765261B5BaEF3" },
+  { name: "USDT", fullname: "Tether", address: "TBA6Yq1ocGwZHVkLwWChYyW1QsrbyzXsrf" },
+  // { name: "ETH", fullname: "Ethereum", address: "0x481A2AD25c61765e26B0A87C69E765261B5BaEF3" },
+  { name: "ETH", fullname: "Ethereum", address: "0x1a2eFB31508F4DEcbB42Bb9aD2710F2f51f76DF3" },
   { name: "TRX", fullname: "Tron", address: "TE4hcP3oPzD8wbcGJbiUHTqFDFM3MgsvBY" },
   { name: "LTC", fullname: "Litecoin", address: "ltc1qy5q0ukrkjndk5fcajv7qdhf9crv6jdfgx4k7qc" },
   {
